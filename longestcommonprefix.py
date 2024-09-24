b9df5daf76678cca763b2dd9769c0f8646679878
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2024-09-24 10:31:31 +0545

    update the prefixes

---
+++ longestcommonprefix.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def longestCommonPrefix(self, strs):
+
+        if not strs:
+            return ''
+
+        prefix = strs[0]
+
+        for i in range(1, len(strs)):
+
+            while strs[i].find(prefix) !=0:
+
+                prefix = prefix[:-1]
+
+                print(prefix)
+
+                if not prefix:
+
+                    return ''
+        return prefix
+
+result = Solution().longestCommonPrefix(['flower', 'flow', 'flight'])
+
+print(result)
